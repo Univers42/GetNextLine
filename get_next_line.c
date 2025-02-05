fec7c1bb341191a3b0e9263480b07202c079e6cf
Dylan Lesieur <dlesieur@c2r1s1.42madrid.com>
2025-02-05 14:45:39 +0100

    advanced word

--- get_next_line.c
+++ get_next_line.c
@@ -1,166 +1,129 @@
-#include <fcntl.h>
-#include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#define BUFFER_SIZE 42
 
-#define BUFFER_SIZE 250
-
-typedef struct s_list
+char *ft_strjoin(char *s1, char *s2)
 {
-    char            *new_line;
-    struct s_list   *next;
-}                   t_list;
+    int len1 = 0;
+    int len2 = 0;
+    char *new;
 
-static t_list *find_last_node(t_list *list)
-{
-    while (list && list->next)
-        list = list->next;
-    return list;
-}
-
-static void free_list(t_list **list)
-{
-    t_list *temp;
-    
-    while (*list)
-    {
-        temp = (*list)->next;
-        free((*list)->new_line);
-        free(*list);
-        *list = temp;
-    }
-}
-
-static int found_newline(t_list *list)
-{
-    while (list)
-    {
-        if (list->new_line && strchr(list->new_line, '\n'))
-            return 1;
-        list = list->next;
-    }
-    return 0;
-}
-
-static void append(t_list **list, char *buf)
-{
-    t_list *new_node = malloc(sizeof(t_list));
-    t_list *last_node;
-    if (!new_node)
-        return;
-    new_node->new_line = buf;
-    new_node->next = NULL;
-    if (!*list)
-        *list = new_node;
+    if (!s1)
+        len1 = 0;
     else
-    {
-        last_node = find_last_node(*list);
-        last_node->next = new_node;
-    }
-}
-
-static void create_list(t_list **list, int fd)
-{
-    char *buf;
-    ssize_t bytes_read;
+        while (s1[len1])
+            len1++;
     
-    while (!found_newline(*list))
-    {
-        buf = malloc(BUFFER_SIZE + 1);
-        if (!buf)
-            return;
-        bytes_read = read(fd, buf, BUFFER_SIZE);
-        if (bytes_read <= 0)
-        {
-            free(buf);
-            return;
-        }
-        buf[bytes_read] = '\0';
-        append(list, buf);
-    }
-}
+    if (!s2)
+        len2 = 0;
+    else
+        while (s2[len2])
+            len2++;
 
-static int len_to_newline(t_list *list)
-{
-    int len = 0;
-    while (list)
-    {
-        for (int i = 0; list->new_line[i]; i++)
-        {
-            if (list->new_line[i] == '\n')
-                return len + 1;
-            len++;
-        }
-        list = list->next;
-    }
-    return len;
-}
-
-static void copy_str(t_list *list, char *dest)
-{
-    int i = 0;
-    while (list)
-    {
-        for (int j = 0; list->new_line[j]; j++)
-        {
-            dest[i++] = list->new_line[j];
-            if (list->new_line[j] == '\n')
-            {
-                dest[i] = '\0';
-                return;
-            }
-        }
-        list = list->next;
-    }
-    dest[i] = '\0';
-}
-
-static void polish_list(t_list **list)
-{
-    t_list *last = find_last_node(*list);
-    if (!last)
-    {
-        free_list(list);
-        return;
-    }
-    char *new_buf = strdup(strchr(last->new_line, '\n') + 1);
-    if (!new_buf)
-    {
-        free_list(list);
-        return;
-    }
-    free_list(list);
-    append(list, new_buf);
-}
-
-char *get_next_line(int fd)
-{
-    static t_list *list = NULL;
-    char *next_line;
-    
-    if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, NULL, 0) < 0)
+    new = malloc(sizeof(char) * (len1 + len2 + 1));
+    if (!new)
         return NULL;
-    create_list(&list, fd);
-    if (!list)
-        return NULL;
-    next_line = malloc(len_to_newline(list) + 1);
-    if (!next_line)
-        return NULL;
-    copy_str(list, next_line);
-    polish_list(&list);
-    return next_line;
+
+    for (int i = 0; i < len1; i++)
+        new[i] = s1[i];
+    for (int i = 0; i < len2; i++)
+        new[i + len1] = s2[i];
+    new[len1 + len2] = '\0';
+
+    free(s1);
+    return new;
 }
 
-int main(int  argc, char *argv)
+static char *ft_strchr(const char *str, int c)
 {
-    if(argc != 2)
-        return  1;
+    while (*str && *str != c)
+        str++;
+    if (*str == c)
+        return (char *)str;
+    return NULL;
+}
+
+int get_next_line(int fd, char **line)
+{
+    static char *stash[1024] = {NULL};
+    char buffer[BUFFER_SIZE + 1];
+    char *tmp;
+    int bytes_read;
+
+    if (fd < 0 || !line || BUFFER_SIZE <= 0)
+        return (-1);
+
+    buffer[0] = '\0';
+    if (stash[fd])
+    {
+        tmp = stash[fd];
+        stash[fd] = ft_strchr(tmp, '\n');
+        if (stash[fd])
+        {
+            *(stash[fd]) = '\0';
+            *line = tmp;
+            stash[fd]++;
+            return (1);
+        }
+        *line = tmp;
+        free(tmp);
+        stash[fd] = NULL;
+        return (1);
+    }
+
+    bytes_read = read(fd, buffer, BUFFER_SIZE);
+    while (bytes_read > 0)
+    {
+        buffer[bytes_read] = '\0';
+        tmp = stash[fd];
+        stash[fd] = ft_strjoin(tmp, buffer);
+
+        if (!stash[fd])
+            return (-1);
+
+        if (ft_strchr(buffer, '\n'))
+            break;
+        bytes_read = read(fd, buffer, BUFFER_SIZE);
+    }
+
+    if (bytes_read == -1)
+        return (-1);
+    if (bytes_read == 0 && !stash[fd])
+        return (0);
+
+    *line = stash[fd];
+    stash[fd] = ft_strchr(*line, '\n');
+    if (stash[fd])
+    {
+        *(stash[fd]) = '\0';
+        stash[fd]++;
+    }
+    else
+        stash[fd] = NULL;
+
+    return (1);
+}
+
+int main(int argc, char **argv)
+{
     int fd;
-    char buffer[BUFFER_SIZE];
-    int nbytes;
+    char *line;
+
+    if (argc != 2)
+        return 1;
+
     fd = open(argv[1], O_RDONLY);
-    while(nbytes = read(fd, buffer ,BUFFER_SIZE))
+    if (fd == -1)
+        return 1;
+
+    while (get_next_line(fd, &line) == 1)
     {
-        
+        printf("%s\n", line);
+        free(line);
     }
+
+    close(fd);
+    return 0;
 }
