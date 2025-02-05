e918e45b875b576a14863ee4c0671ed5f4521744
Dylan Lesieur <dlesieur@c2r1s1.42madrid.com>
2025-02-05 14:56:53 +0100

    issue with  buffer, use of enqueue dequeu approach

--- get_next_line.c
+++ get_next_line.c
@@ -1,131 +1,137 @@
-#include <unistd.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 #include <fcntl.h>
-#define BUFFER_SIZE 42
 
-char *ft_strjoin(char *s1, char *s2)
-{
-    int len1 = 0;
-    int len2 = 0;
-    char *new;
+#define BUFFER_SIZE 1024
 
-    if (!s1)
-        len1 = 0;
-    else
-        while (s1[len1])
-            len1++;
+typedef struct s_queue_node {
+    char *data;
+    struct s_queue_node *next;
+} t_queue_node;
+
+typedef struct s_queue {
+    t_queue_node *head;
+    t_queue_node *tail;
+} t_queue;
+
+t_queue *queue_init(void) {
+    t_queue *queue = malloc(sizeof(t_queue));
+    if (!queue)
+        return NULL;
+    queue->head = NULL;
+    queue->tail = NULL;
+    return queue;
+}
+
+void enqueue(t_queue *queue, const char *data) {
+    t_queue_node *node = malloc(sizeof(t_queue_node));
+    if (!node)
+        return;
+    node->data = strdup(data);
+    node->next = NULL;
     
-    if (!s2)
-        len2 = 0;
+    if (!queue->head)
+        queue->head = node;
     else
-        while (s2[len2])
-            len2++;
+        queue->tail->next = node;
+    queue->tail = node;
+}
 
-    new = malloc(sizeof(char) * (len1 + len2 + 1));
+char *dequeue(t_queue *queue) {
+    if (!queue->head)
+        return NULL;
+    
+    t_queue_node *temp = queue->head;
+    char *data = temp->data;
+    queue->head = queue->head->next;
+    if (!queue->head)
+        queue->tail = NULL;
+    
+    free(temp);
+    return data;
+}
+
+char *join_and_free(char *s1, const char *s2) {
+    size_t len1 = s1 ? strlen(s1) : 0;
+    size_t len2 = s2 ? strlen(s2) : 0;
+    char *new = malloc(len1 + len2 + 1);
     if (!new)
         return NULL;
-
-    for (int i = 0; i < len1; i++)
-        new[i] = s1[i];
-    for (int i = 0; i < len2; i++)
-        new[i + len1] = s2[i];
-    new[len1 + len2] = '\0';
-
-    free(s1);
+    
+    if (s1) {
+        strcpy(new, s1);
+        free(s1);
+    }
+    strcpy(new + len1, s2);
+    
     return new;
 }
 
-static char *ft_strchr(const char *str, int c)
-{
-    while (*str && *str != c)
-        str++;
-    if (*str == c)
-        return (char *)str;
-    return NULL;
+char *find_newline(char **stash) {
+    if (!*stash)
+        return NULL;
+    
+    char *newline_pos = strchr(*stash, '\n');
+    if (!newline_pos)
+        return NULL;
+    
+    size_t line_length = newline_pos - *stash + 1;
+    char *line = strndup(*stash, line_length);
+    
+    char *new_stash = strdup(newline_pos + 1);
+    free(*stash);
+    *stash = new_stash;
+    
+    return line;
 }
 
-int get_next_line(int fd, char **line)
-{
+void process_buffer(char **stash, const char *buffer, int bytes_read) {
+    buffer[bytes_read] = '\0';
+    *stash = join_and_free(*stash, buffer);
+}
+
+char *get_next_line(int fd) {
     static char *stash[1024] = {NULL};
     char buffer[BUFFER_SIZE + 1];
-    char *tmp;
     int bytes_read;
-
-    if (fd < 0 || !line || BUFFER_SIZE <= 0)
-        return (-1);
-
-    buffer[0] = '\0';
-    if (stash[fd])
-    {
-        tmp = stash[fd];
-        stash[fd] = ft_strchr(tmp, '\n');
-        if (stash[fd])
-        {
-            *(stash[fd]) = '\0';
-            *line = tmp;
-            stash[fd]++;
-            return (1);
-        }
-        *line = tmp;
-        free(tmp);
-        stash[fd] = NULL;
-        return (1);
-    }
-
-    bytes_read = read(fd, buffer, BUFFER_SIZE);
-    while (bytes_read > 0)
-    {
-        buffer[bytes_read] = '\0';
-        tmp = stash[fd];
-        stash[fd] = ft_strjoin(tmp, buffer);
-
-        if (!stash[fd])
-            return (-1);
-
-        if (ft_strchr(buffer, '\n'))
-            break;
+    
+    if (fd < 0 || BUFFER_SIZE <= 0)
+        return NULL;
+    
+    while (1) {
         bytes_read = read(fd, buffer, BUFFER_SIZE);
+        if (bytes_read == -1)
+            return NULL;
+        if (bytes_read == 0 && (!stash[fd] || !*stash[fd]))
+            return NULL;
+        
+        process_buffer(&stash[fd], buffer, bytes_read);
+        char *line = find_newline(&stash[fd]);
+        if (line)
+            return line;
     }
-
-    if (bytes_read == -1)
-        return (-1);
-    if (bytes_read == 0 && !stash[fd])
-        return (0);
-
-    *line = stash[fd];
-    stash[fd] = ft_strchr(*line, '\n');
-    if (stash[fd])
-    {
-        *(stash[fd]) = '\0';
-        stash[fd]++;
-    }
-    else
-        stash[fd] = NULL;
-
-    return (1);
 }
 
-int main(int argc, char **argv)
-{
-    int fd;
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+        return 1;
+    }
+    
+    int fd = open(argv[1], O_RDONLY);
+    if (fd < 0) {
+        perror("Error opening file");
+        return 1;
+    }
+    
     char *line;
-
-    if (argc != 2)
-        return 1;
-
-    fd = open(argv[1], O_RDONLY);
-    if (fd == -1)
-        return 1;
-
-    while (get_next_line(fd, &line) == 1)
-    {
-        printf("%s\n", line);
+    while ((line = get_next_line(fd))) {
+        printf("%s", line);
         free(line);
     }
-
+    
     close(fd);
     return 0;
 }
-
-//error
