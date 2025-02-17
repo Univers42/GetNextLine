ead02c8e69acad5390f7b931c6fc02ac3964eb3c
LESdylan <dev.pro.photo@gmail.com>
2025-02-17 23:14:25 +0100

    update

--- approach/string/get_next_line_bonus1.c
+++ approach/string/get_next_line_bonus1.c
@@ -4,7 +4,7 @@
 #include <string.h>
 
 #ifndef MAX_FD
-#define MAX_FD 1024  // Define a maximum number of file descriptors to track
+    #define MAX_FD 1024  // Define a maximum number of file descriptors to track
 #endif
 
 // Helper function to find newline position
@@ -17,7 +17,7 @@ static int find_newline(char *str)
     while (*ptr)
     {
         if (*ptr == '\n')
-            return ptr - str;  // Return the index of the newline
+            return (ptr - str);  // Return the index of the newline
         ptr++;
     }
     return -1;
@@ -75,7 +75,7 @@ static char *get_line(char *memory)
     memmove(line, memory, newline_idx + 1);
     line[newline_idx + 1] = '\0';
 
-    return line;
+    return (line);
 }
 
 // Keeps only the leftover part of memory after '\n'
@@ -108,11 +108,85 @@ char *get_next_line(int fd)
     if (fd < 0 || fd >= MAX_FD || BUFFER_SIZE <= 0)
         return NULL;
 
-    memory[fd] = store_chunks(fd, memory[fd]);
-    if (!memory[fd])
+    *(memory + fd) = store_chunks(fd, *(memory + fd));
+    if (!*(memory + fd))
         return NULL;
 
-    line = get_line(memory[fd]);
-    memory[fd] = get_leftover(memory[fd]);
+    line = get_line(*(memory + fd));
+    *(memory + fd) = get_leftover(*(memory + fd));
     return line;
+}
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include "get_next_line.h"
+
+int main(void)
+{
+    char *line;
+    int fd1, fd2;
+    int i;
+
+    // Test 1: Reading from a regular file
+    printf("Test 1: Reading from regular file\n");
+    fd1 = open("test.txt", O_RDONLY);
+    if (fd1 == -1)
+    {
+        printf("Error opening test.txt\n");
+        return (1);
+    }
+
+    i = 1;
+    while ((line = get_next_line(fd1)) != NULL)
+    {
+        printf("Line %d: %s", i, line);
+        free(line);
+        i++;
+    }
+    printf("End of file reached\n");
+    close(fd1);
+
+    // Test 2: Multiple file descriptors (bonus part)
+    printf("\nTest 2: Multiple file descriptors\n");
+    fd1 = open("test1.txt", O_RDONLY);
+    fd2 = open("test2.txt", O_RDONLY);
+    if (fd1 == -1 || fd2 == -1)
+    {
+        printf("Error opening test files\n");
+        return (1);
+    }
+
+    i = 1;
+    while ((line = get_next_line(fd1)) != NULL || 
+           (line = get_next_line(fd2)) != NULL)
+    {
+        if (line != NULL)
+        {
+            printf("Line %d: %s", i, line);
+            free(line);
+            i++;
+        }
+    }
+    printf("End of both files reached\n");
+    close(fd1);
+    close(fd2);
+
+    // Test 3: Empty file
+    printf("\nTest 3: Empty file\n");
+    fd1 = open("empty.txt", O_RDONLY);
+    if (fd1 == -1)
+    {
+        printf("Error opening empty.txt\n");
+        return (1);
+    }
+
+    line = get_next_line(fd1);
+    if (line == NULL)
+        printf("Successfully handled empty file\n");
+    else
+        printf("Error: Empty file test failed\n");
+    close(fd1);
+
+    return (0);
 }
